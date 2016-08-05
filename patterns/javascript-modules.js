d73eb10806734de94ae0ce530925e8f1dc0032e2
Scott Bradley <scottbradley.mail@gmail.com>
2016-08-05 15:34:38 -0500

    moves javacript modules into patterns dir

--- patterns/javascript-modules.js
+++ patterns/javascript-modules.js
@@ -3,22 +3,22 @@ from Addy Osmani's Learning JavaScript Design Patterns (https://addyosmani.com/r
 */
 // Global module
 var myModule = (function ( jQ, _ ) {
- 
+
     function privateMethod1(){
         jQ(".container").html("test");
     }
- 
+
     function privateMethod2(){
       console.log( _.min([10, 5, 100, 2, 1000]) );
     }
- 
+
     return{
         publicMethod: function(){
             privateMethod1();
         }
     };
- 
+
 // Pull in jQuery and Underscore
 })( jQuery, _ );
- 
+
 myModule.publicMethod();
